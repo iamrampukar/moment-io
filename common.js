2eddf7d4ea972483f26eab55a44067f538924445
Ram Pukar <ram_mosone@yahoo.com>
2021-03-19 18:08:46 +0545

    Year Difference result show

--- common.js
+++ common.js
@@ -1,5 +1,13 @@
 moment.locale('ja');
 $(document).ready(function () {
-    const defaultDateFormat = moment().format('LL');
+    const defaultFormat = "YYYY-MM-DD HH:mm:ss";
+    const yearFormat = "YYYY-MM-DD";
+    const nowDate = new Date();
+    const defaultDateFormat = moment(nowDate).format(defaultFormat);
     $('#defaultDateFormat').html(defaultDateFormat);
+
+    // const startDate = moment('1995-01-01', 'YYYY-MM-DD');
+    // const endDate = moment('2021-05-15', 'YYYY-MM-DD');
+    // const yearDiff = endDate.diff(startDate, 'years');
+    // $('#yearDifference').html(`startDate: ${startDate}, endDate: ${endDate} => <strong>${yearDiff} Years</strong>`);
 })
